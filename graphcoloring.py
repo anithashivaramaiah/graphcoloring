428fdf39d59fe478c17fdff7ee2857f50edda116
Anitha Shivaramaiah <31722178+anithashivaramaiah@users.noreply.github.com>
2022-02-16 15:26:07 -0600

    Update graphcoloring.py

--- graphcoloring.py
+++ graphcoloring.py
@@ -1,51 +1,50 @@
 def isItSafe(graphs, colors):
-
-	for i in range(4):
-		for j in range(i + 1, 4):
-			if (graphs[i][j] and colors[j] == colors[i]):
-				return False
-	return True
+    for i in range(5):
+        for j in range(i + 1, 5):
+            if (graphs[i][j] and colors[j] == colors[i]):
+                return False
+    return True
 
 
 def ColoringOfGraph(graphs, m, i, colors):
-	
-	if (i == 4):
+    if (i == 5):
 
-		if (isItSafe(graphs, colors)):
+        if (isItSafe(graphs, colors)):
+            # Print the solution
+            printSolution(colors)
+            return True
+        return False
 
-			# Print the solution
-			printSolution(colors)
-			return True
-		return False
+    for j in range(1, m + 1):
+        colors[i] = j
 
-	for j in range(1, m + 1):
-		colors[i] = j
+        if (ColoringOfGraph(graphs, m, i + 1, colors)):
+            return True
+        colors[i] = 0
+    return False
 
-		
-		if (ColoringOfGraph(graphs, m, i + 1, colors)):
-			return True
-		colors[i] = 0
-	return False
 
 def printSolution(colors):
-	print("Solution does Exists:" " Following are colors assigned to the graph ")
-	for i in range(4):
-		print(colors[i],end=" ")
+    print("Solution does Exists for this graph:" " Following are colors assigned to the graph ")
+    for i in range(5):
+        print(colors[i], end=" ")
+
 
 if __name__ == '__main__':
-	
-	# creating the graph as mentioned in the paper, with 4 vertices and we use 3 colors to color the graph
-	graphs = [
-		[ 0, 1, 1, 1 ],
-		[ 1, 0, 1, 0 ],
-		[ 1, 1, 0, 1 ],
-		[ 1, 0, 1, 0 ],
-	]
-	m = 3 
-	colors = [0 for i in range(4)]
 
-	if (not ColoringOfGraph(graphs, m, 0, colors)):
-		print ("Solution for the given graph does not exist")
+    # creating the graph as mentioned in the paper, with 5 vertices and we use 3 colors to color the graph
+    graphs = [
+        [0, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0],
+        [1, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 1, 0]
+    ]
+    m = 3
+    colors = [0 for i in range(5)]
 
-#code by AnithaShivaramaiah
+    if (not ColoringOfGraph(graphs, m, 0, colors)):
+        print("Solution for the given graph does not exist")
+
+# code by AnithaShivaramaiah
 
